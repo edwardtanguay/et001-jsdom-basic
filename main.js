6b1c5bed2a1595503ce0fa55eb92a78e5f5c98aa
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-23 06:47:39 +0200

    button works

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const messageElem = document.querySelector('.message');
+const changeMessageButtonElem = document.querySelector('.changeMessageButton');
 
 messageElem.innerHTML = 'NEW MESSAGE';
+
+changeMessageButtonElem.addEventListener('click', () => {
+	messageElem.innerText = 'Welcome to this interactive website.';
+	messageElem.style.color = 'yellow';
+});
